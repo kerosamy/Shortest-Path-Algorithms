719ce3a66845d8e50afab401969d78d92fe108be
caroline-2 <carolinehanna510@gmail.com>
2025-04-13 21:13:33 +0200

    Dijkstra done

--- src/main/java/main/example/Main.java
+++ src/main/java/main/example/Main.java
@@ -1,8 +1,8 @@
 package main.example;
 
 import main.example.ShortestPathAlgo.Pair;
+import main.example.ShortestPathAlgo.Dijkstra;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,7 +15,7 @@ public class Main {
         for (int i = 0; i < numberOfNodes; i++) {
             adjList.add(new ArrayList<>());
         }
-        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\kero samy\\OneDrive - Alexandria University\\Desktop\\shortest path algorithms\\src\\main\\java\\main\\example\\ShortestPathAlgo\\Dijkstra"));
+        BufferedReader br = new BufferedReader(new FileReader("/media/caroline/New Volume/4th term/Data structures/lab2/Shortest-Path-Algorithms/src/main/java/main/example/ShortestPathAlgo/Dijkstra"));
         String line;
         while ((line = br.readLine()) != null) {
             String[] parts = line.trim().split("\\s+");
@@ -33,6 +33,33 @@ public class Main {
             }
             System.out.println();
         }
+        Dijkstra dijkstra = new Dijkstra();
+        int start = 1;
+        dijkstra.shortestPathRun(adjList, start);
+        int end = 5;
+        System.out.println("Distance from " + start + " to " + end + ": " + dijkstra.shortestPathDistance(start, end));
+        System.out.println("Path from " + start + " to " + end + ": " + dijkstra.shortestPath(start, end));
+
+        Dijkstra dijkstra1 = new Dijkstra();
+        int start1 = 2;
+        dijkstra1.shortestPathRun(adjList, start1);
+        int end1 = 5;
+        System.out.println("Distance from " + start1 + " to " + end1 + ": " + dijkstra1.shortestPathDistance(start1, end1));
+        System.out.println("Path from " + start1 + " to " + end1 + ": " + dijkstra1.shortestPath(start1, end1));
+
+        Dijkstra dijkstra2 = new Dijkstra();
+        int start2 = 10;
+        dijkstra2.shortestPathRun(adjList, start2);
+        int end2 = 5;
+        System.out.println("Distance from " + start2 + " to " + end2+ ": " + dijkstra2.shortestPathDistance(start2, end2));
+        System.out.println("Path from " + start2 + " to " + end2 + ": " + dijkstra2.shortestPath(start2, end2));
+
+        Dijkstra dijkstra3= new Dijkstra();
+        int start3 = 7;
+        dijkstra3.shortestPathRun(adjList, start3);
+        int end3 = 8;
+        System.out.println("Distance from " + start3 + " to " + end3 + ": " + dijkstra3.shortestPathDistance(start3, end3));
+        System.out.println("Path from " + start3 + " to " + end3 + ": " + dijkstra3.shortestPath(start3, end3));
 
     }
 }
---
+++ src/main/java/main/example/ShortestPathAlgo/Dijkstra.java
@@ -0,0 +1,57 @@
+package main.example.ShortestPathAlgo;
+
+import java.util.*;
+
+public class Dijkstra implements ShortestPathStrategy {
+    private int[] dist;
+    private int[] parent;
+    private int n;
+    @Override
+    public void shortestPathRun(ArrayList<ArrayList<Pair<Integer,Integer>>>graph,int start) {
+        this.n=graph.size();
+        dist=new int[n];
+        parent=new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        Arrays.fill(parent,-1);
+        boolean[]visited=new boolean[n];
+        dist[start]=0;
+        PriorityQueue<Pair<Integer,Integer>>pq=new PriorityQueue<>(Comparator.comparingInt(a -> a.first));
+        pq.add(new Pair<>(0,start));
+        while (!pq.isEmpty()) {
+            Pair<Integer, Integer>current=pq.poll();
+            int node=current.second;
+            int distance=current.first;
+            if (visited[node]) continue;
+            visited[node]=true;
+            for (Pair<Integer,Integer> y:graph.get(node)) {
+                int v=y.first;
+                int weight=y.second;
+                if (!visited[v] && dist[node]+weight<dist[v]) {
+                    dist[v]=dist[node]+weight;
+                    parent[v]=node;
+                    pq.add(new Pair<>(dist[v],v));
+                }
+            }
+        }
+    }
+
+    @Override
+    public String shortestPath(int start, int end) {
+        if (dist[end]==Integer.MAX_VALUE) {
+            return " ";
+        }
+        List<Integer>path=new ArrayList<>();
+        int x=end;
+        while(x!=-1){
+            path.add(Integer.valueOf(x));
+            x=parent[x];
+        }
+        Collections.reverse(path);
+        return path.toString();
+    }
+
+    @Override
+    public int shortestPathDistance(int start, int end) {
+        return dist[end]==Integer.MAX_VALUE ? -1 : dist[end];
+    }
+}
